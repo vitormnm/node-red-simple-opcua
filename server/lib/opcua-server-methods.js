0a746478156b708a2c9fccd457d4307b4583e9a5
vitor <vitor@aa.com>
2026-05-28 13:38:50 -0300

    new version

--- server/lib/opcua-server-methods.js
+++ server/lib/opcua-server-methods.js
@@ -112,7 +112,7 @@ class OpcUaServerMethods {
             (inputArguments, context, callback) => {
                 const eventId = inputArguments[0].value;
                 const comment = inputArguments[1].value;
-
+                
                 const alarm = context.object;
                 // severity atual
                 const severity = alarm.severity.readValue().value.value;
